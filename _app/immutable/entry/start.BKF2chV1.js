be6a80d6ba0f66fbf06781ac3fd7d28cd62bb448
rogo! <r480github@gmail.com>
2026-08-18 05:21:42 -0700

    fix wisp

--- _app/immutable/entry/start.BKF2chV1.js
+++
@@ -1,1 +0,0 @@
-import{l as o,s as r}from"../chunks/Ca7vnsXR.js";export{o as load_css,r as start};
---
+++ _app/immutable/entry/start.QiyZYp5N.js
@@ -0,0 +1,1 @@
+import{l as o,s as r}from"../chunks/Dm_2qBtC.js";export{o as load_css,r as start};
