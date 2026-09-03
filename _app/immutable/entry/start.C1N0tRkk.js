324caee788edb9e7e68be4dbeff22ac1e0d5e115
rogo! <r480github@gmail.com>
2026-09-02 21:43:29 -0700

    svg points to slate

--- _app/immutable/entry/start.C1N0tRkk.js
+++
@@ -1,1 +0,0 @@
-import{s as r}from"../chunks/skGc1FRi.js";import{m as t}from"../chunks/Ahp82ZWI.js";export{t as load_css,r as start};
---
+++ _app/immutable/entry/start.ZaWvGxxZ.js
@@ -0,0 +1,1 @@
+import{s as r}from"../chunks/BbY6jba7.js";import{m as t}from"../chunks/B3OC-Ew3.js";export{t as load_css,r as start};
